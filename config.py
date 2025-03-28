5467ebc556274b3e54c6aafc4e7c955661677192
Evgen-Jekov <zekovevgen992@gmail.com>
2025-03-28 00:05:19 +0000

    I study fixed acceptable values

---
+++ config.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class TaskModel(str, Enum):
+    id_task = 'id_task'
+    name_task = 'name_task'
+
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from config import TaskModel
 
 app = FastAPI(debug=True)
 
-@app.get('/task/{id}')
-async def root(id : int):
-    return {'msg' : id}
+@app.get('/task/p{model}')
+async def root(model : TaskModel):
+    if model is TaskModel.id_task:
+        return {'SUCCESS' : 'you us id task'}
+    elif model is TaskModel.name_task:
+        return {'SUCCESS' : 'you us name task'}
